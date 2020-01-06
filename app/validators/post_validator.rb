ae5a4266d47640c06916bc43a2b4cf0480cf459c
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:06:24 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,7 +1,7 @@
 class PostValidator < ActiveModel::Validator
   
   def validate(record)
-    unless record.title =~ ("Won't Believe", "Secret", "Top [number]", "Guess")
+    unless record.title =~ /(Won't Believe|Secret|Guess|Top \d)/
 
 
 end
