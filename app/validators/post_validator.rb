bbf278aaa1626502974a61bde4c84b127cf487ff
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:07:35 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -2,7 +2,7 @@ class PostValidator < ActiveModel::Validator
   
   def validate(record)
     unless record.title =~ /(Won't Believe|Secret|Guess|Top \d)/
-        record.errors[:title]<<
+        record.errors[:title]<< "moar clickbait pl0x"
     end
   end
 
