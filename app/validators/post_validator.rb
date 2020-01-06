0d14b0a811b26a821b9ae73214c0c6f753c369b3
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:06:54 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -2,6 +2,9 @@ class PostValidator < ActiveModel::Validator
   
   def validate(record)
     unless record.title =~ /(Won't Believe|Secret|Guess|Top \d)/
+        record.errors
+    end
+  end
 
 
 end
