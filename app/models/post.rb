6633a321ca99b00e22606d940fa12fa24ca1f2fc
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:03:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   include ActiveModel::Validations
-  validates_with EmailValidator
+  validates_with PostValidator
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
