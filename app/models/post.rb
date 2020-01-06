9920808525dee9e9d78fa72aa0a6fdefb581d7e3
John Rogala <johnrossrogala@gmail.com>
2020-01-06 22:35:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :title_must_be_clickbaity
+   
+  def title_must_be_clickbaity
+    list = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    
+    if title.present? && unless title.any?("Won't Believe", "Secret", "Top [number]", "Guess")
+      errors.add(:title, "must be clickbaity")
+    end
+  end
+end
+  
 end
